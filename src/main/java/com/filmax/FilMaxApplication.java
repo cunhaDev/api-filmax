adba47e17dfb1ced6d0a80a4c59a764aae537b42
cunhaDev <dev.mateuscunha@gmail.com>
2021-04-02 16:49:18 -0400

    first commit project Open Source Filmax

---
+++ src/main/java/com/filmax/FilMaxApplication.java
@@ -0,0 +1,13 @@
+package com.filmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilMaxApplication.class, args);
+	}
+
+}
