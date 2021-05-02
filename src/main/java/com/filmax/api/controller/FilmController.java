544a5e1428573a651dc4ae2badca0c6fdec145f9
cunhaDev <dev.mateuscunha@gmail.com>
2021-05-01 20:19:38 -0400

    add new feature

--- src/main/java/com/filmax/api/controller/FilmController.java
+++ src/main/java/com/filmax/api/controller/FilmController.java
@@ -25,7 +25,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping({ "/api" })
+@RequestMapping({ "/repolinks" })
 @Api(value = "Projeto Open Source Filmax")
 @CrossOrigin(origins = "*")
 public class FilmController {
